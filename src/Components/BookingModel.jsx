fdb4531cd6ab2ca8638cb8db52f0b3bc4fe7ce91
Sheetal_V <sheetalvbagal10@gmail.com>
2023-02-13 17:02:01 +0530

    public branch

---
+++ src/Components/BookingModel.jsx
@@ -0,0 +1,24 @@
+import React from 'react'
+import Typography from '@mui/material/Typography';
+
+
+
+function BookingModel() {
+    // const style= StyleSheet.create({
+    //     img1:{
+    //         width: 100, height:100, marginTop:50, alignSelf:'center'
+    //     }
+    // })
+
+  return (
+    <div>
+      <Typography   title= {'Book Appointment'}
+       id="modal-modal-title" variant="h6" component="h2">
+            Text in a modal
+          </Typography>
+          {/* <img source={require('')} style={style.img1}/> */}
+    </div>
+  )
+}
+
+export default BookingModel
