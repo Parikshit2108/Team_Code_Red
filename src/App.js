bff9ca848086b21d5c154980f3ba8994aefc9b25
manithirumalokonda <manisimple444@gmail.com>
2023-02-16 10:02:17 +0530

    channge

--- src/App.js
+++ src/App.js
@@ -1,14 +1,21 @@
-import React from 'react'
-// import PlansPrice from '../src/plansprice/PlansPrice'
-import PlanpriceCard from './plansprice/PlanpriceCard'
+import { createTheme } from "@mui/material";
+// import { ThemeProvider } from "@mui/styles";
+// import "bootstrap/dist/css/bootstrap.min.css";
+// import Login from "./Components/Login"
+// import About from "./Components/About"
+// import { BrowserRouter, Route, Routes } from "react-router-dom";
+import "./App.css";
+// import Sidebar from "./Components/Sidebar";
+import PlanpriceCard from "./plansprice/PlanpriceCard";
+
+const theme = createTheme({});
 
 function App() {
   return (
-    <div>
-      <PlanpriceCard />
-
+    <div className="App">
+     <PlanpriceCard />
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
